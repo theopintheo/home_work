e3ce1eb0d71aa796a52bdf39792dff93f551e1c1
theopin <theopintheo@gmail.com>
2025-09-16 19:40:28 +0530

    uptated

---
+++ stringEX4.js
@@ -0,0 +1,3 @@
+let text1 = "Hello world, welcome to the world of programming!";
+let indexOfWorld =text1.indexOf("world") // YOUR CODE HERE
+console.log("Exercise 4 Index:", indexOfWorld); // Expected: 6
